e379b8fce85873eee4d35caa5cc06a5fc1b68cd6
MAINAK CHAUDHURI <64016811+MainakRepositor@users.noreply.github.com>
2022-03-03 07:51:52 +0530

    Update typed.js

--- assets/vendor/typed.js/typed.js
+++ assets/vendor/typed.js/typed.js
@@ -1,12 +1,4 @@
-/*!
- * 
- *   typed.js - A JavaScript Typing Animation Library
- *   Author: Matt Boldt <me@mattboldt.com>
- *   Version: v2.0.12
- *   Url: https://github.com/mattboldt/typed.js
- *   License(s): MIT
- * 
- */
+
 (function webpackUniversalModuleDefinition(root, factory) {
 	if(typeof exports === 'object' && typeof module === 'object')
 		module.exports = factory();
